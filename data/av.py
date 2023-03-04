aa277a3060e62d9260b64609c5f24d9ffc5a8aef
Zmicier <39563347+ZmicierGT@users.noreply.github.com>
2023-03-04 17:10:47 +0700

    AV stocks basics refactoring

--- data/av.py
+++ data/av.py
@@ -23,49 +23,9 @@ from data.futils import get_ts_from_str
 
 import settings
 
-class AVType(str, Enum):
-    """
-        Enumeration with AlphaVantage functions.
-    """
-    Intraday = 'TIME_SERIES_INTRADAY'
-    IntradayExtended = 'TIME_SERIES_INTRADAY_EXTENDED'
-    Daily = 'TIME_SERIES_DAILY'
-    Weekly = 'TIME_SERIES_WEEKLY'
-    WeeklyAdjusted = 'TIME_SERIES_WEEKLY_ADJUSTED'
-    Monthly = 'TIME_SERIES_MONTHLY'
-    MontlyAdjusted = 'TIME_SERIES_MONTHLY_ADJUSTED'
-    Currency = 'CURRENCY_EXCHANGE_RATE'
-    FxIntraday = 'FX_INTRADAY'
-    FxDaily = 'FX_DAILY'
-    FxWeekly = 'FX_WEEKLY'
-    FxMonthly = 'FX_MONTHLY'
-    Crypto = 'CURRENCY_EXCHANGE_RATE'
-    CryptoIntraday = 'CRYPTO_INTRADAY'
-    CryptoDaily = 'DIGITAL_CURRENCY_DAILY'
-    CryptoWeekly = 'DIGITAL_CURRENCY_WEEKLY'
-    CryptoMonthly = 'DIGITAL_CURRENCY_MONTHLY'
+# TODO Add unit test for this module
 
-class AVResultsCI(str, Enum):
-    """
-        Enumeration with AlphaVantage results for crypto intraday.
-    """
-    Open = '1. open'
-    High = '2. high'
-    Low = '3. low'
-    Close = '4. close'
-    Volume = '5. volume'
-
-class AVResultsCD(str, Enum):
-    """
-        Enumeration with AlphaVantage results for crypto daily.
-    """
-    Open = '1a. open (USD)'
-    High = '2a. high (USD)'
-    Low = '3a. low (USD)'
-    Close = '4a. close (USD)'
-    Volume = '5. volume'
-
-class AV(fdata.BaseFetchData):
+class AVStock(fdata.BaseFetchData):
     """
         AlphaVantage API wrapper class.
     """
@@ -75,8 +35,8 @@ class AV(fdata.BaseFetchData):
 
         # Default values
         self.source_title = "AlphaVantage"
-        self.api_key = settings.AV.api_key  # Please note that free keys do not support live quotes now.
-        self.type = AVType.Daily
+        self.api_key = settings.AV.api_key
+        self.compact = True  # Indicates if a limited number (100) of quotes should be obtained
 
         if self.api_key is None:
             raise FdataError("API key is needed for this data source. Get your free API key at alphavantage.co and put it in setting.py")
@@ -103,8 +63,7 @@ class AV(fdata.BaseFetchData):
             return '30min'
         elif self.timespan == Timespans.Hour:
             return '60min'
-        elif self.timespan == Timespans.Day:
-            return ''  # Daily quotes are obtained with different function and there is no timespan parameter.
+        # Other timespans are obtained with different function and there is no timespan parameter.
         else:
             raise FdataError(f"Unsupported timespan: {self.timespan}")
 
@@ -116,15 +75,36 @@ class AV(fdata.BaseFetchData):
                 list: quotes data
 
             Raises:
-                FdataError: incorrect API key(limit reached), http error happened or no data obtained.
+                FdataError: incorrect API key(limit reached), http error happened, invalid timespan or no data obtained.
         """
-        url = f"https://www.alphavantage.co/query?function={self.type}&symbol={self.symbol}&market=USD&interval={self.get_timespan()}&apikey={self.api_key}"
+        # At first, need to set a function depending on a timespan.
+        if self.timespan == Timespans.Day:
+            output_size = 'compact' if self.compact else 'full'
+            json_key = 'Time Series (Daily)'
 
-        AVResults = AVResultsCI
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={self.symbol}&outputsize={output_size}&apikey={self.api_key}'
+        elif self.timespan == Timespans.Week:
+            json_key = 'Weekly Adjusted Time Series'
 
-        if self.type == AVType.CryptoDaily:
-            AVResults = AVResultsCD
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={self.symbol}&apikey={self.api_key}'
+        elif self.timespan == Timespans.Month:
+            json_key = 'Monthly Adjusted Time Series'
 
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={self.symbol}&apikey={self.api_key}'
+        # All intraday timespans
+        elif self.timespan in [Timespans.Minute,
+                               Timespans.FiveMinutes,
+                               Timespans.FifteenMinutes,
+                               Timespans.ThirtyMinutes,
+                               Timespans.Hour]:
+            output_size = 'compact' if self.compact else 'full'
+            json_key = f'Time Series ({self.get_timespan()})'
+
+            url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={self.symbol}&interval={self.get_timespan()}&outputsize={output_size}&apikey={self.api_key}'
+        else:
+            raise FdataError(f"Unsupported timespan: {self.timespan}")
+
+        # Get quotes data
         try:
             response = requests.get(url, timeout=30)
         except (urllib.error.HTTPError, urllib.error.URLError, http.client.HTTPException) as e:
@@ -132,13 +112,7 @@ class AV(fdata.BaseFetchData):
 
         json_data = response.json()
 
-        try:
-            key = list(json_data.keys())[1]
-        except IndexError as e:
-            raise FdataError(f"Can't get quotes. Maybe API key limit is reached: {e}") from e
-
-        dict_results = json_data[key]
-
+        dict_results = json_data[json_key]
         datetimes = list(dict_results.keys())
 
         if len(datetimes) == 0:
@@ -152,28 +126,41 @@ class AV(fdata.BaseFetchData):
             except ValueError as e:
                 raise FdataError(f"Can't parse the datetime {dt_str}: {e}") from e
 
-            # Keep all non-intraday timestamps at 23:59:59
-            if self.timespan in (Timespans.Day, Timespans.Week, Timespans.Month, Timespans.Year):
-                # Keep all datetimes UTC adjusted
-                dt = datetime.utcfromtimestamp(ts)
-                dt = dt.replace(tzinfo=pytz.utc)
-                dt = dt.replace(hour=23, minute=59, second=59)
-                ts = int(datetime.timestamp(dt))
+            # Keep all datetimes UTC adjusted
+            dt = datetime.utcfromtimestamp(ts)
+            dt = dt.replace(tzinfo=pytz.utc)
 
+            # The current quote to process
             quote = dict_results[dt_str]
 
             quote_dict = {
-                "v": quote[AVResults.Volume],
-                "o": quote[AVResults.Open],
-                "c": quote[AVResults.Close],
-                "h": quote[AVResults.High],
-                "l": quote[AVResults.Low],
-                "cl": "NULL",
-                "n": "NULL",
-                "vw": "NULL",
-                "d": "NULL",
-                "t": ts
+                't': None,
+                'o': quote['1. open'],
+                'h': quote['2. high'],
+                'l': quote['3. low'],
+                'c': None,
+                'cl': None,
+                'v': None,
+                'd': None,
+                'n': None,
+                'vw': None
             }
+
+            # Set the entries depending if the quote is intraday
+            if self.timespan in (Timespans.Day, Timespans.Week, Timespans.Month):
+                quote_dict['c'] = quote['5. adjusted close']
+                quote_dict['cl'] = quote['4. close']
+                quote_dict['v'] = quote['6. volume']
+                quote_dict['d'] = quote['7. dividend amount']
+
+                # Keep all non-intraday timestamps at 23:59:59
+                dt = dt.replace(hour=23, minute=59, second=59)
+            else:
+                quote_dict['c'] = quote['4. close']
+                quote_dict['v'] = quote['5. volume']
+
+            # Set the timestamp
+            quote_dict['t'] = int(datetime.timestamp(dt))
 
             quotes_data.append(quote_dict)
 

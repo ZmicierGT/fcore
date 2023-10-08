1c39548bf020bfb20334cb069864fb88b5f78776
Zmicier <39563347+ZmicierGT@users.noreply.github.com>
2023-10-08 23:14:58 +0700

    Demos improvements

--- quickstart/min_growth_probability.py
+++ quickstart/min_growth_probability.py
@@ -65,13 +65,19 @@ class Probability(Classifier):
 # Demonstration
 ###############
 
-threshold_learn, threshold_test = 5284, 565  # Quotes num thresholds for the learning and testing
 period_long, period_short = (50, 25)  # Periods for SMAs
+threshold_learn, threshold_test = 5284, 565  # Quotes num thresholds in db for the learning and testing
+threshold_divs_learn, threshold_divs_test = 124, 8
+threshold_splits = 0
 
 # Get data for training/testing a model with the number of quotes >= threshold
 # All the data will be cached in a database without the need of further fetching
-rows_learn, length_learn = YF(symbol='SPY', first_date="2000-1-1", last_date="2021-1-1").fetch_if_none(threshold_learn)
-rows_test, length_test = YF(symbol='SPY', first_date="2021-1-2", last_date="2023-4-1").fetch_if_none(threshold_test)
+rows_learn, length_learn = \
+    YF(symbol='SPY', first_date="2000-1-1", last_date="2021-1-1").\
+    fetch_stock_data_if_none(threshold_learn, threshold_divs_learn, threshold_splits)
+rows_test, length_test = \
+    YF(symbol='SPY', first_date="2021-1-2", last_date="2023-4-1").\
+        fetch_stock_data_if_none(threshold_test, threshold_divs_test, threshold_splits)
 
 prob = Probability(period_long=period_long,
                    period_short=period_short,

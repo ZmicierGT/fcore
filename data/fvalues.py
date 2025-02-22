fc268b496f30187d1cc8d466b05023896d14386a
Zmicier <39563347+ZmicierGT@users.noreply.github.com>
2025-02-22 16:06:15 +0700

    Index composition advancements (SW20 added, DJIA improved)

--- data/fvalues.py
+++ data/fvalues.py
@@ -51,9 +51,13 @@ djia_aug_31_2020 = ['MMM', 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'K
 djia_feb_26_2024 = ['MMM', 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'KO', 'DIS', 'DOW', 'GS', 'HD', 'HON', 'IBM',\
                     'INTC', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT', 'NKE', 'PG', 'CRM', 'TRV', 'UNH', 'VZ', 'V', 'AMZN', 'WMT']
 
+djia_nov_08_2024 = ['MMM', 'AXP', 'AMGN', 'AAPL', 'BA', 'CAT', 'CVX', 'CSCO', 'KO', 'DIS', 'SHW', 'GS', 'HD', 'HON', 'IBM',\
+                    'NVDA', 'JNJ', 'JPM', 'MCD', 'MRK', 'MSFT', 'NKE', 'PG', 'CRM', 'TRV', 'UNH', 'VZ', 'V', 'AMZN', 'WMT']
+
 # Note that each time sorting of the resulting list may be different
 djia_combined = sorted(list(set(djia_jun_08_2009 + djia_sep_24_2012 + djia_sep_23_2013 + djia_mar_19_2015 + \
-                                djia_jun_26_2018 + djia_apr_02_2019 + djia_apr_06_2020 + djia_feb_26_2024)))
+                                djia_jun_26_2018 + djia_apr_02_2019 + djia_apr_06_2020 + djia_feb_26_2024 + \
+                                djia_nov_08_2024)))
 
 djia_combined.remove('UTX')  # No historical quotes for these delisted symbols in the supported data sources
 djia_combined.remove('KRFT')
@@ -66,13 +70,245 @@ djia_dict = {'2009-06-08': djia_jun_08_2009,
              '2019-04-02': djia_apr_02_2019,
              '2020-04-06': djia_apr_06_2020,
              '2020-08-31': djia_aug_31_2020,
-             '2024-02-26': djia_feb_26_2024}
+             '2024-02-26': djia_feb_26_2024,
+             '2024-11-08': djia_nov_08_2024}
 
 # The current DJIA composition (EDOW for equal weighted ETF for comparison, DIA for Price weighted)
-djia = djia_feb_26_2024
+djia = djia_nov_08_2024
 
 sector_titles = ['Technology', 'Financial Services', 'Healthcare', 'Consumer Cyclical', 'Industrials', \
                  'Communication Services', 'Consumer Defensive', 'Energy', 'Basic Materials', 'Real Estate', 'Utilities']
+
+########################################
+# SW20 (Swiss Market Index) related data
+########################################
+
+# ETFs for comparison:
+# XSMI.SW - CHF Dist (1/2008)
+# XSMC.SW - CHF Acc (7/2013)
+# EWL - USD Dist (Likely MSCI Switzerland based, 3/1996)
+
+# The following tickers are missed from yfinance / FMP data:
+# Credit Suisse Group - CSGN.SW / CS
+# Syngenta - SYNN.SW / SYT (ADR ticker is taken by other company)
+# Actelion - ATLN.SW / ALIOY (No ADR data on FMP)
+# Transocean - RIGN.SW / RIG (ADR present on yfinance)
+# Synthes - SYST.SW / SYSTY (No ADR data on FMP)
+# Nobel Biocare - NOBN.SW / NBHYY (No ADR data on FMP)
+
+# Credit Suisse Group (CSGN.SW) was replaced by Kuehne + Nagel International AG (KNIN.SW)
+sw20_06_jun_23 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'SIKA.SW', \
+                  'ALC.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'PGHN.SW', 'SREN.SW', 'SOON.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'LOGN.SW', 'KNIN.SW']
+
+# SGS SA (SGSN.SW) was replaced with Sonova (SOON.SW)
+sw20_24_sep_22 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'SIKA.SW', \
+                  'ALC.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'PGHN.SW', 'SREN.SW', 'SOON.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'LOGN.SW', 'CSGN.SW']
+
+# Swatch Group (UHR.SW) was replaced with Logitech (LOGN.SW)
+sw20_05_sep_21 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'SIKA.SW', \
+                  'ALC.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'PGHN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'LOGN.SW', 'CSGN.SW']
+
+# Skipped (May 2021) as the only change was LafargeHolcim renamed to Holcim.
+
+# Adecco (ADEN.SW) replaced by Partners Group (PGHN.SW)
+sw20_03_sep_20 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'SIKA.SW', \
+                  'ALC.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'PGHN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'UHR.SW', 'CSGN.SW']
+
+# Julius Bar (BAER.SW) replaced by Alcon (ALC.SW)
+sw20_27_mar_19 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'SIKA.SW', \
+                  'ALC.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'UHR.SW', 'CSGN.SW']
+
+# Sika AG (SIKA.SW) replaced Syngenta (SYNN.SW) after its purchase by ChemChina
+sw20_05_may_17 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'SIKA.SW', \
+                  'BAER.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'UHR.SW', 'CSGN.SW']
+
+# Actelion (ATLN.SW) replaced by Lonza Group (LONN.SW) after Actelion's purchase by Johnson & Johnson
+sw20_24_apr_17 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'LONN.SW', 'BAER.SW', \
+                  'SYNN.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'UHR.SW', 'CSGN.SW']
+
+# Transocean (RIGN.SW) replaced by Swiss Life (SLHN.SW)
+sw20_14_jan_16 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'BAER.SW', 'SYNN.SW', \
+                  'ATLN.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'SLHN.SW', \
+                  'UHR.SW', 'CSGN.SW']
+
+# Synthes (SYST.SW) replaced by Geberit (GEBN.SW) due to the merger with Johnson & Johnson
+sw20_18_jun_12 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'BAER.SW', 'SYNN.SW', \
+                  'ATLN.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'GEBN.SW', 'UHR.SW', \
+                  'CSGN.SW', 'RIGN.SW']
+
+# Lonza Group (LONN.SW) replaced by Givaudan (GIVN.SW)
+sw20_16_sep_11 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'BAER.SW', 'SYNN.SW', \
+                  'ATLN.SW', 'GIVN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'UHR.SW', 'CSGN.SW', \
+                  'RIGN.SW', 'SYST.SW']
+
+# Swiss Life (SLHN.SW) replaced by Transocean (RIGN.SW)
+sw20_21_jun_10 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'BAER.SW', 'SYNN.SW', \
+                  'ATLN.SW', 'LONN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'UHR.SW', 'CSGN.SW', \
+                  'SYST.SW', 'RIGN.SW']
+
+# Nobel Biocare (NOBN.SW) and Baloise (BALN.SW) replaced by Lonza Group (LONN.SW) and SGS (SGSN.SW)
+sw20_21_sep_09 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'BAER.SW', 'SYNN.SW', \
+                  'ATLN.SW', 'LONN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'SGSN.SW', 'UHR.SW', 'SLHN.SW', \
+                  'CSGN.SW', 'SYST.SW']
+
+sw20_05_jul_07 = ['NESN.SW', 'ROG.SW', 'NOVN.SW', 'CFR.SW', 'ZURN.SW', 'UBSG.SW', 'ABBN.SW', 'BAER.SW', 'SYNN.SW', \
+                  'ATLN.SW', 'HOLN.SW', 'SCMN.SW', 'ADEN.SW', 'SREN.SW', 'BALN.SW', 'UHR.SW', 'SLHN.SW', 'CSGN.SW', \
+                  'SYST.SW', 'NOBN.SW']
+
+sw20 = sw20_06_jun_23
+
+sw20_combined = sorted(list(set(sw20_05_jul_07 + sw20_21_sep_09 + sw20_21_jun_10 + sw20_16_sep_11 + \
+                                sw20_18_jun_12 + sw20_14_jan_16 + sw20_24_apr_17 + sw20_27_mar_19 + \
+                                sw20_03_sep_20 + sw20_05_sep_21 + sw20_24_sep_22 + sw20_06_jun_23)))
+
+sw20_combined_yf = sw20_combined
+
+# No data at yfinance
+sw20_combined_yf.remove('ATLN.SW')
+sw20_combined_yf.remove('CSGN.SW')
+sw20_combined_yf.remove('NOBN.SW')
+sw20_combined_yf.remove('RIGN.SW')
+sw20_combined_yf.remove('SYNN.SW')
+sw20_combined_yf.remove('SYST.SW')
+
+sw20_dict = {'2007-07-05': sw20_05_jul_07,
+             '2009-09-21': sw20_21_sep_09,
+             '2010-06-21': sw20_21_jun_10,
+             '2011-09-16': sw20_16_sep_11,
+             '2012-06-18': sw20_18_jun_12,
+             '2016-01-14': sw20_14_jan_16,
+             '2017-04-24': sw20_24_apr_17,
+             '2019-03-27': sw20_27_mar_19,
+             '2020-09-20': sw20_03_sep_20,
+             '2021-09-05': sw20_05_sep_21,
+             '2022-09-24': sw20_24_sep_22,
+             '2023-06-06': sw20_06_jun_23}
+
+# Use like return sw20_adrs.get(input, "Not found") to get an entry or
+# [sw20_adrs.get(item, item) for item in the_list] to 'convert' the whole list to ADRs
+sw20_adrs = {
+    'ABBN.SW':  'ABBNY',  # 4/2001
+    'ADEN.SW':  'AHEXY',  # 12/2009
+    'ALC.SW':   'ALC',    # 4/2019
+    'BAER.SW':  'JBAXY',  # 4/2010
+    'BALN.SW':  'BLHEY',  # 10/2009 No ADR but in index since 2007
+    'CFR.SW':   'CFRUY',  # 12/2009 No ADR but in index since 2007
+    'GEBN.SW':  'GBERY',  # 10/2011
+    'GIVN.SW':  'GVDNY',  # 12/2008
+    'HOLN.SW':  'HCMLY',  # 1/2010
+    'KNIN.SW':  'KHNGY',  # 3/2012
+    'LOGN.SW':  'LOGI',   # 3/1997
+    'LONN.SW':  'LZAGY',  # 1/2010
+    'NESN.SW':  'NSRGY',  # 11/1996
+    'NOVN.SW':  'NVS',    # 11/1996
+    'PGHN.SW':  'PGPHF',  # 1/2015
+    'ROG.SW':   'RHHBY',  # 8/2003
+    'SCMN.SW':  'SCMWY',  # 10/1998
+    'SGSN.SW':  'SGSOY',  # 1/2008
+    'SIKA.SW':  'SXYAY',  # 11/2015
+    'SLHN.SW':  'SZLMY',  # 5/2009
+    'SOON.SW':  'SONVY',  # 4/2011
+    'SREN.SW':  'SSREY',  # 5/2011
+    'UBSG.SW':  'UBS',    # 5/2000
+    'UHR.SW':   'SWGAY',  # 10.2010
+    'ZURN.SW':  'ZURVY',  # 6/2003
+    'CSGN.SW':  'CS',     # Delisted
+    'SYNN.SW':  'SYT',    # Delisted
+    'ATLN.SW':  'ALIOY',  # Delisted
+    'RIGN.SW':  'RIG',    # Delisted
+    'SYST.SW':  'SYSTY',  # Delisted
+    'NOBN.SW':  'NBHYY'   # Delisted
+}
+
+# Credit Suisse Group (CS) was replaced by Kuehne + Nagel International AG (KHNGY)
+sw20_adr_06_jun_23 = [sw20_adrs.get(item, item) for item in sw20_06_jun_23]
+
+# SGS SA (SGSOY) was replaced with Sonova (SONVY)
+sw20_adr_24_sep_22 = [sw20_adrs.get(item, item) for item in sw20_24_sep_22]
+
+# Swatch Group (SWGAY) was replaced with Logitech (LOGI)
+sw20_adr_05_sep_21 = [sw20_adrs.get(item, item) for item in sw20_05_sep_21]
+
+# Skipped (May 2021) as the only change was LafargeHolcim renamed to Holcim.
+
+# Adecco (AHEXY) replaced by Partners Group (PGPHF)
+sw20_adr_03_sep_20 = [sw20_adrs.get(item, item) for item in sw20_03_sep_20]
+
+# Julius Bar (JBAXY) replaced by Alcon (ALC)
+sw20_adr_27_mar_19 = [sw20_adrs.get(item, item) for item in sw20_27_mar_19]
+
+# Sika AG (SXYAY) replaced Syngenta (SYT) after its purchase by ChemChina
+sw20_adr_05_may_17 = [sw20_adrs.get(item, item) for item in sw20_05_may_17]
+
+# Actelion (ALIOY) replaced by Lonza Group (LZAGY) after Actelion's purchase by Johnson & Johnson
+sw20_adr_24_apr_17 = [sw20_adrs.get(item, item) for item in sw20_24_apr_17]
+
+# Transocean (RIG) replaced by Swiss Life (SLHN)
+sw20_adr_14_jan_16 = [sw20_adrs.get(item, item) for item in sw20_14_jan_16]
+
+# Synthes (SYSTY) replaced by Geberit (GBERY) due to the merger with Johnson & Johnson
+sw20_adr_18_jun_12 = [sw20_adrs.get(item, item) for item in sw20_18_jun_12]
+
+# Lonza Group (LZAGY) replaced by Givaudan (GVDNY)
+sw20_adr_16_sep_11 = [sw20_adrs.get(item, item) for item in sw20_16_sep_11]
+
+# Swiss Life (SLHN) replaced by Transocean (RIG)
+sw20_adr_21_jun_10 = [sw20_adrs.get(item, item) for item in sw20_21_jun_10]
+
+# Nobel Biocare (NBHYY) and Baloise (BLHEY) replaced by Lonza Group (LZAGY) and SGS (SGSOY)
+sw20_adr_21_sep_09 = [sw20_adrs.get(item, item) for item in sw20_21_sep_09]
+
+sw20_adr_05_jul_07 = [sw20_adrs.get(item, item) for item in sw20_05_jul_07]
+
+sw20_adr = sw20_adr_06_jun_23
+
+sw20_adr_combined = sorted(list(set(sw20_adr_05_jul_07 + sw20_adr_21_sep_09 + sw20_adr_21_jun_10 + sw20_adr_16_sep_11 + \
+                                    sw20_adr_18_jun_12 + sw20_adr_14_jan_16 + sw20_adr_24_apr_17 + sw20_adr_27_mar_19 + \
+                                    sw20_adr_03_sep_20 + sw20_adr_05_sep_21 + sw20_adr_24_sep_22 + sw20_adr_06_jun_23)))
+
+sw20_adr_combined_fmp = sw20_adr_combined
+
+# No data on FMP
+sw20_adr_combined_fmp.remove('ALIOY')
+sw20_adr_combined_fmp.remove('SYSTY')
+sw20_adr_combined_fmp.remove('NBHYY')
+
+# Ticker is taken by another company
+sw20_adr_combined_fmp.remove('SYT')
+
+sw20_adr_combined_yf = sw20_adr_combined_fmp
+
+# No data on yahoo finance
+sw20_adr_combined_yf.remove('CS')
+
+sw20_adr_dict = {'2007-07-05': sw20_adr_05_jul_07,
+                 '2009-09-21': sw20_adr_21_sep_09,
+                 '2010-06-21': sw20_adr_21_jun_10,
+                 '2011-09-16': sw20_adr_16_sep_11,
+                 '2012-06-18': sw20_adr_18_jun_12,
+                 '2016-01-14': sw20_adr_14_jan_16,
+                 '2017-04-24': sw20_adr_24_apr_17,
+                 '2019-03-27': sw20_adr_27_mar_19,
+                 '2020-09-20': sw20_adr_03_sep_20,
+                 '2021-09-05': sw20_adr_05_sep_21,
+                 '2022-09-24': sw20_adr_24_sep_22,
+                 '2023-06-06': sw20_adr_06_jun_23}
+
+##############################
+# Canadian Big-Five/Six banks:
+##############################
+ca_big_five = ['RY.TO', 'TD.TO', 'BNS.TO', 'BMO.TO', 'CM.TO']  # 1/1995
+ca_big_five_adr = ['RY', 'TD', 'BNS', 'BMO', 'CM']  # 6/2002
+
+ca_big_six = ca_big_five + ['NA.TO']  # 1/1995
+ca_big_six_adr = ca_big_five_adr + ['NTIOF']  # 4/2010
 
 ###############################
 # Below are sectoral ETFs lists
